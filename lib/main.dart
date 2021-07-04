122e6ef05c23f8b5f6dd969b268842da38ac60f7
Maruf Khan <maruf1609@gmail.com>
2021-07-04 17:35:02 +0600

    GridView.Count implemented

--- lib/main.dart
+++ lib/main.dart
@@ -5,53 +5,90 @@ void main() {
     title: 'Stack',
     home: Scaffold(
       appBar: AppBar(
-        title: Text('Stack'),
+        title: Text('Grid'),
       ),
       body: Home(),
     ),
-
   ));
 }
+
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Stack(
-        //used clipBehavior Clip.none instead of overflow.visible which is deprecated
-        clipBehavior: Clip.hardEdge,
-        // mainAxisAlignment: MainAxisAlignment.center,
-        //alignment: Alignment.center,
-        children: [
-          Container(
-            width: 200,
-            height: 200,
-            color: Colors.red,
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            child: Container(
-
-              width: 100,
-              height: 100,
-              color: Colors.blue,
-            ),
-          ),
-          Positioned(
-            bottom: -25,
-            right: 10,
-            child: Container(
-              width: 50,
-              height: 50,
-              color: Colors.green,
-            ),
-          ),
-        ],
-      ),
-
+    return GridView.count(
+      crossAxisCount: 3,
+      mainAxisSpacing: 10,
+      crossAxisSpacing: 10,
+      children: [
+        Container(
+          color: Colors.red,
+          child: Text('Grid'),
+          alignment: Alignment.center,
+        ),
+        Container(
+          color: Colors.blue,
+          child: Text('Grid'),
+          alignment: Alignment.center,
+        ),
+        Container(
+          color: Colors.green,
+          child: Text('Grid'),
+          alignment: Alignment.center,
+        ),
+        Container(
+          color: Colors.red,
+          child: Text('Grid'),
+          alignment: Alignment.center,
+        ),
+        Container(
+          color: Colors.blue,
+          child: Text('Grid'),
+          alignment: Alignment.center,
+        ),
+        Container(
+          color: Colors.green,
+          child: Text('Grid'),
+          alignment: Alignment.center,
+        ),
+      ],
     );
   }
 }
 
+//  return Center(
+//       child: Stack(
+//         //used clipBehavior Clip.none instead of overflow.visible which is deprecated
+//         clipBehavior: Clip.hardEdge,
+//         // mainAxisAlignment: MainAxisAlignment.center,
+//         //alignment: Alignment.center,
+//         children: [
+//           Container(
+//             width: 200,
+//             height: 200,
+//             color: Colors.red,
+//           ),
+//           Positioned(
+//             bottom: 0,
+//             left: 0,
+//             child: Container(
+//
+//               width: 100,
+//               height: 100,
+//               color: Colors.blue,
+//             ),
+//           ),
+//           Positioned(
+//             bottom: -25,
+//             right: 10,
+//             child: Container(
+//               width: 50,
+//               height: 50,
+//               color: Colors.green,
+//             ),
+//           ),
+//         ],
+//       ),
+//
+//     );
