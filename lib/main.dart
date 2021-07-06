8447e03887c21d07d0e55641285dc9e6b04cdfca
Maruf Khan <maruf1609@gmail.com>
2021-07-06 12:35:50 +0600

    Card widget implemented

--- lib/main.dart
+++ lib/main.dart
@@ -12,92 +12,28 @@ void main() {
   ));
 }
 
-class Home extends StatefulWidget {
+class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  List<int> list = [];
-
-  @override
-  void initState() {
-    super.initState();
-    for(int i=0; i<=50;i++){
-      list.add(i);
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      itemCount: list.length,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-        itemBuilder: (BuildContext cx, int index){
-        return Padding(
-          padding: const EdgeInsets.all(10),
-          child: Container(
-            color: Colors.blue,
-            child: Text('${list[index]}'),
-            alignment: Alignment.center,
-          ),
-        );
-        });
+    return Card(
+      color: Colors.blue,
+      elevation: 5,
+      shadowColor: Colors.yellow,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(50),
+        side: BorderSide(
+          color: Colors.red,
+          width: 2,
+        ),
+      ),
+      child: Container(
+        width: 300,
+        height: 300,
+
+      ),
+    );
   }
 }
 
-
-
-//GridView.count(
-//       crossAxisCount: 2,
-//       mainAxisSpacing: 10,
-//       crossAxisSpacing: 10,
-//       children: [
-//         Container(
-//           color: Colors.red,
-//           child: Text('Grid'),
-//           alignment: Alignment.center,
-//         ),
-//         Container(
-//           color: Colors.blue,
-//           child: Text('Grid'),
-//           alignment: Alignment.center,
-//         ),
-//         Container(
-//           color: Colors.green,
-//           child: Text('Grid'),
-//           alignment: Alignment.center,
-//         ),Container(
-//           color: Colors.red,
-//           child: Text('Grid'),
-//           alignment: Alignment.center,
-//         ),
-//         Container(
-//           color: Colors.blue,
-//           child: Text('Grid'),
-//           alignment: Alignment.center,
-//         ),
-//         Container(
-//           color: Colors.green,
-//           child: Text('Grid'),
-//           alignment: Alignment.center,
-//         ),
-//         Container(
-//           color: Colors.red,
-//           child: Text('Grid'),
-//           alignment: Alignment.center,
-//         ),
-//         Container(
-//           color: Colors.blue,
-//           child: Text('Grid'),
-//           alignment: Alignment.center,
-//         ),
-//         Container(
-//           color: Colors.green,
-//           child: Text('Grid'),
-//           alignment: Alignment.center,
-//         ),
-//       ],
-//     );
